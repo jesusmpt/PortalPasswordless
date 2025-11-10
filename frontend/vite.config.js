28bf7649e0068cba7dd1d6c285961990773b6b59
jesusmpt <52034316+jesusmpt@users.noreply.github.com>
2025-11-10 17:59:16 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [], // dejar vacío, a menos que quieras excluir librerías externas
-    },
-  },
+  base: '/'  // IMPORTANTE: para Azure SWA
 });
