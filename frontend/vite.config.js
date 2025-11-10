b1308767b0eb5bb34df3aa8d07754e6418a5e799
jesusmpt <52034316+jesusmpt@users.noreply.github.com>
2025-11-10 19:08:54 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './'  // IMPORTANTE para Azure SWA
+    root: './',   // raíz de frontend
+    base: './'    // para rutas relativas
 });
